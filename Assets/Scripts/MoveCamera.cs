c2fdb13daf51e73b3640c54716d1d111aa47311d
kmcgarry1 <kevin.mc.garry.13@gmail.com>
2025-12-04 12:37:21 +0000

    Initial commit - Unity project with player movement

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  public Transform cameraPosition;
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.position = cameraPosition.position;
+  }
+}
